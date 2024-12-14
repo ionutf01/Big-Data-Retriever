8691dfa9006b93bb738b4b61981b1b03ebd90161
nastasevalentin <nastasevalentin23@yahoo.com>
2024-12-14 18:12:43 +0200

    feat: added asp net core solution with initial UI pages

---
+++ Birt/Birt.App/wwwroot/js/site.js
@@ -0,0 +1,57 @@
+﻿function getWorksOfArt() {
+    const dropdown = document.getElementById("queryDropdown");
+    const selectedArtist = dropdown.value;
+
+    if (selectedArtist === "gogh") {
+        getWorksOfArtGogh(); 
+    } else if (selectedArtist === "vinci") {
+        getWorksOfArtDaVinci(); 
+    }
+}
+
+async function getWorksOfArtGogh() {
+    const query = `
+        SELECT ?work ?workLabel WHERE {
+          ?work wdt:P170 wd:Q5582.  
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+        }
+        LIMIT 10
+    `;
+    const url = "https://query.wikidata.org/sparql?query=" + encodeURIComponent(query);
+    const response = await fetch(url, {
+        headers: {
+            'Accept': 'application/sparql-results+json'
+        }
+    });
+    const data = await response.json();
+    displayResults(data);
+}
+
+function displayResults(data) {
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+    data.results.bindings.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item.workLabel.value;
+        resultsContainer.appendChild(listItem);
+    });
+}
+
+
+async function getWorksOfArtDaVinci() {
+    const query = `
+        SELECT ?work ?workLabel WHERE {
+          ?work wdt:P170 wd:Q762.  # Work of art by Vincent van Gogh (Q5582)
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+        }
+        LIMIT 10
+    `;
+    const url = "https://query.wikidata.org/sparql?query=" + encodeURIComponent(query);
+    const response = await fetch(url, {
+        headers: {
+            'Accept': 'application/sparql-results+json'
+        }
+    });
+    const data = await response.json();
+    displayResults(data);
+}
